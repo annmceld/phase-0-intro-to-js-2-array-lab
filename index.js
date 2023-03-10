e07f91dee8112a6c6636b38cf630c6d940794df5
Ann McElderry <thayerann1@gmail.com>
2023-03-10 11:43:23 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    console.log(cats.push(name));
+}
+function destructivelyPrependCat(name) {
+    console.log(cats.unshift(name));
+}
+function destructivelyRemoveLastCat(name) {
+    console.log(cats.pop(name));
+}
+function destructivelyRemoveFirstCat(name) {
+    console.log(cats.shift(name));
+}
+function appendCat(name) {
+    return [...cats, name];
+}
+function prependCat(name) {
+    return [name, ...cats];
+}
+function removeLastCat(name) {
+    return cats.slice(0, 2);
+}
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
